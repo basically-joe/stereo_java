71d96f9662a1a83a91ad70e1eed2d4e28860cb8d
Joe McColl <joe4kool@gmail.com>
2019-02-19 12:13:42 +0000

    passes cd testing

--- src/main/java/CD.java
+++ src/main/java/CD.java
@@ -1,2 +1,3 @@
-public class CD {
+public class CD extends Component {
+
 }
--- src/test/java/CDTest.java
+++ src/test/java/CDTest.java
@@ -1,2 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+
 public class CDTest {
+
+	CD cd;
+
+	@Before
+	public void before(){
+		cd = new CD();
+	}
+
+
+	@Test
+	public void canGetCurrentlyPlayingNull() {
+		assertEquals(null, cd.getCurrentlyPlaying());
+	}
+
+	@Test
+	public void canSetCurrentlyPlaying() {
+		cd.setCurrentlyPlaying("Hurt");
+		assertEquals("Hurt", cd.getCurrentlyPlaying());
+	}
+
+	@Test
+	public void canGetPowerStartsOff(){
+		assertEquals(false, cd.isPowerOnOff());
+	}
+
+	@Test
+	public void canTurnPowerOn(){
+		cd.setPowerOnOff(true);
+		assertEquals(true, cd.isPowerOnOff());
+	}
+	@Test
+	public void canTurnPowerOff(){
+		cd.setPowerOnOff(false);
+		assertEquals(false, cd.isPowerOnOff());
+	}
+
 }
